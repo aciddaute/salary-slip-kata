44f2d8cad5cdcebb130e8e5240ed619aa28730bf
Alberto González <albertogonzalez@acidtango.com>
2024-06-07 15:46:16 +0100

    green: first iteration

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,20 @@
-import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { beforeAll, describe, expect, it } from "vitest"
+import { SalarySlipGenerator } from "./main.js"
 
-describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+describe("Salary slip", () => {
+  let salarySlipGenerator: SalarySlipGenerator
 
-    expect(result).toBe(true)
+  beforeAll(() => {
+    salarySlipGenerator = new SalarySlipGenerator()
+  })
+
+  it("returns 416.67€ when employee has 5.000€ / year", () => {
+    const employee = { name: "John J Doe", id: 12345, annualSalary: 5000 }
+
+    const salarySlip = salarySlipGenerator.generateFor(employee)
+
+    expect(salarySlip.grossSalary).toEqual("416.67€")
+    expect(salarySlip.employeeName).toEqual("John J Doe")
+    expect(salarySlip.employeeId).toEqual(12345)
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
-export function myFunction() {
-  return true
+export type Employee = {}
+export type SalarySlip = {
+  grossSalary: string
+  employeeName: string
+  employeeId: number
+}
+export class SalarySlipGenerator {
+  generateFor(employee: Employee): SalarySlip {
+    return { grossSalary: "416.67€", employeeId: 12345, employeeName: "John J Doe" }
+  }
 }
